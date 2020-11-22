5ad42431463470d6d80e33a2c90ebd6cb6698cdd
Filipe GP <eastrall@users.noreply.github.com>
2020-11-22 19:29:02 +0100

    Add base solution

---
+++ src/Redstone.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redstone.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

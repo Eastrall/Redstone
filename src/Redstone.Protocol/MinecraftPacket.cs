cddfda873370f3f1a2e747f98665d2a7356d6502
Filipe GP <eastrall@users.noreply.github.com>
2020-11-27 09:18:02 +0100

    Begin of minecraft protocol and unit tests

---
+++ src/Redstone.Protocol/MinecraftPacket.cs
@@ -0,0 +1,94 @@
+﻿using LiteNetwork.Protocol;
+using Redstone.Protocol.Abstractions;
+using System;
+
+namespace Redstone.Protocol
+{
+    public class MinecraftPacket : LitePacket, IMinecraftPacket
+    {
+        public int PacketId { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="MinecraftPacket"/> in write-only mode.
+        /// </summary>
+        public MinecraftPacket(int packetId)
+        {
+            PacketId = packetId;
+            WriteVarInt32(packetId);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="MinecraftPacket"/> in read-only mode.
+        /// </summary>
+        /// <param name="buffer">Packet buffer data.</param>
+        public MinecraftPacket(int packetId, byte[] buffer) 
+            : base(buffer)
+        {
+            PacketId = packetId;
+        }
+
+        public Guid ReadUUID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ReadVarInt32()
+        {
+            int numRead = 0;
+            int result = 0;
+            byte read;
+
+            do
+            {
+                read = ReadByte();
+                int value = (read & 0b01111111);
+                result |= (value << (7 * numRead));
+
+                numRead++;
+                if (numRead > 5)
+                {
+                    throw new InvalidOperationException("VarInt32 is too big.");
+                }
+            } while ((read & 0b10000000) != 0);
+
+            return result;
+        }
+
+        public long ReadVarInt64()
+        {
+            int numRead = 0;
+            long result = 0;
+            byte read;
+
+            do
+            {
+                read = ReadByte();
+                long value = (read & 0b01111111);
+                result |= (value << (7 * numRead));
+
+                numRead++;
+                if (numRead > 10)
+                {
+                    throw new InvalidOperationException("VarInt64 is too big.");
+                }
+            } while ((read & 0b10000000) != 0);
+
+            return result;
+        }
+
+        public void WriteUUID(Guid value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteVarInt32(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteVarInt64(long value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Redstone.Server/MinecraftUser.cs
+++ src/Redstone.Server/MinecraftUser.cs
@@ -1,10 +1,27 @@
-﻿using LiteNetwork.Server;
+﻿using LiteNetwork.Protocol.Abstractions;
+using LiteNetwork.Server;
+using Redstone.Protocol.Abstractions;
 using System;
+using System.Threading.Tasks;
 
 namespace Redstone.Server
 {
     public class MinecraftUser : LiteServerUser
     {
+        public override Task HandleMessageAsync(ILitePacketStream incomingPacketStream)
+        {
+            if (incomingPacketStream is not IMinecraftPacket packet)
+            {
+                return Task.CompletedTask;
+            }
+
+            int protocolVersion = packet.ReadVarInt32();
+
+            Console.WriteLine($"Protocol version: {protocolVersion}");
+            
+            return base.HandleMessageAsync(incomingPacketStream);
+        }
+
         protected override void OnConnected()
         {
             Console.WriteLine($"New client connected with id: '{Id}'.");

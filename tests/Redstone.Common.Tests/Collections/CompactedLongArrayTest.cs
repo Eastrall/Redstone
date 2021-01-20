5e7f291bb9a99f1a1fffc222ad26150dfa199d78
Filipe GP <eastrall@users.noreply.github.com>
2021-01-20 18:22:06 +0100

    Add compacted long array

--- tests/Redstone.Common.Tests/Collections/CompactedLongArrayTest.cs
+++ tests/Redstone.Common.Tests/Collections/CompactedLongArrayTest.cs
@@ -1,6 +1,9 @@
-﻿using Redstone.Common.Collections;
+﻿using Bogus;
+using Redstone.Common.Collections;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -8,16 +11,30 @@ namespace Redstone.Common.Tests.Collections
 {
     public class CompactedLongArrayTest
     {
-        [Fact]
-        public void Test()
+        [Theory]
+        [InlineData(4, 4096, 4)]
+        [InlineData(4, 4096, 8)]
+        [InlineData(4, 4096, 10)]
+        [InlineData(4, 4096, 12)]
+        [InlineData(4, 4096, 15)]
+        [InlineData(8, 1024, 4)]
+        [InlineData(8, 1024, 8)]
+        [InlineData(8, 1024, 10)]
+        [InlineData(8, 1024, 12)]
+        [InlineData(8, 1024, 15)]
+        public void Test(byte bitsPerEntry, int capacity, int value)
         {
-            var array = new CompactedLongArray(4, 4096);
+            var array = new CompactedLongArray(bitsPerEntry, capacity);
 
-            array[0] = 3;
-            array[1] = 42;
+            for (int i = 0; i < capacity; i++)
+            {
+                array[i] = value;
+            }
 
-            var v = array[0];
-            var v2 = array[1];
+            for (int i = 0; i < capacity; i++)
+            {
+                Assert.Equal(value, array[i]);
+            }
         }
     }
 }

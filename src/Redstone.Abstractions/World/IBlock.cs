9083fd90852121acf413f0b12005b2c6dc0b8015
Filipe GP <eastrall@users.noreply.github.com>
2021-02-27 11:06:58 +0100

    Block fluid check

--- src/Redstone.Abstractions/World/IBlock.cs
+++ src/Redstone.Abstractions/World/IBlock.cs
@@ -9,6 +9,11 @@ namespace Redstone.Abstractions.World
         /// Gets a boolean value that indicates if the current block is an air block.
         /// </summary>
         bool IsAir { get; }
+
+        /// <summary>
+        /// Gets a boolean value that indicates if the current block is a fluid block.
+        /// </summary>
+        bool IsFluid { get; }
         
         /// <summary>
         /// Gets the block type.

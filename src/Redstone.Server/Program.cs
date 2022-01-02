6a7d61d6afcf57465b9afd2eaff59dea1afc1e2f
Filipe GP <4021025+Eastrall@users.noreply.github.com>
2022-01-02 19:34:23 +0100

    Add minimal API support and remove NLOG

--- src/Redstone.Server/Program.cs
+++ src/Redstone.Server/Program.cs
@@ -5,64 +5,54 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog.Extensions.Hosting;
 using Redstone.Abstractions;
 using Redstone.Common.Configuration;
 using Redstone.Common.DependencyInjection;
 using Redstone.Common.Extensions;
 using Redstone.Protocol;
+using Redstone.Server;
 using System;
 using System.Reflection;
-using System.Threading.Tasks;
 
-namespace Redstone.Server
-{
-    class Program
+IHost serverHost = new HostBuilder()
+    .ConfigureAppConfiguration((host, config) =>
     {
-        static Task Main()
+        config.SetBasePath(EnvironmentExtensions.GetCurrentEnvironementDirectory());
+        config.AddYamlFile("config/server.yml", optional: false, reloadOnChange: true);
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureLogging(builder =>
+    {
+        builder.AddConsole();
+        builder.AddFilter("LiteNetwork", LogLevel.Warning);
+        builder.SetMinimumLevel(LogLevel.Trace);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddOptions();
+        services.Configure<ServerOptions>(context.Configuration.GetSection("server"));
+        services.Configure<GameOptions>(context.Configuration.GetSection("game"));
+        services.AddInjectableServices(Assembly.GetExecutingAssembly());
+        services.AddMinecraftProtocol(Assembly.GetExecutingAssembly());
+        services.UseLiteNetwork(builder =>
         {
-            IHost serverHost = new HostBuilder()
-                .ConfigureAppConfiguration((host, config) =>
-                {
-                    config.SetBasePath(EnvironmentExtensions.GetCurrentEnvironementDirectory());
-                    config.AddYamlFile("config/server.yml", optional: false, reloadOnChange: true);
-                    config.AddEnvironmentVariables();
-                })
-                .ConfigureLogging(builder =>
-                {
-                    builder.AddFilter("LiteNetwork", LogLevel.Warning);
-                    builder.SetMinimumLevel(LogLevel.Trace);
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddOptions();
-                    services.Configure<ServerOptions>(context.Configuration.GetSection("server"));
-                    services.Configure<GameOptions>(context.Configuration.GetSection("game"));
-                    services.AddInjectableServices(Assembly.GetExecutingAssembly());
-                    services.AddMinecraftProtocol(Assembly.GetExecutingAssembly());
-                    services.UseLiteNetwork(builder =>
-                    {
-                        builder.AddLiteServer<IRedstoneServer, RedstoneServer, MinecraftUser>(options =>
-                        {
-                            var serverConfiguration = context.Configuration.GetSection("server").Get<ServerOptions>();
+            builder.AddLiteServer<IRedstoneServer, RedstoneServer, MinecraftUser>(options =>
+            {
+                var serverConfiguration = context.Configuration.GetSection("server").Get<ServerOptions>();
 
-                            if (serverConfiguration is null)
-                            {
-                                throw new InvalidProgramException($"Failed to load server settings.");
-                            }
+                if (serverConfiguration is null)
+                {
+                    throw new InvalidProgramException($"Failed to load server settings.");
+                }
 
-                            options.Host = serverConfiguration.Ip;
-                            options.Port = serverConfiguration.Port;
-                            options.PacketProcessor = new MinecraftPacketProcessor();
-                            options.ReceiveStrategy = ReceiveStrategyType.Queued;
-                        });
-                    });
-                })
-                .UseNLog()
-                .UseConsoleLifetime()
-                .Build();
+                options.Host = serverConfiguration.Ip;
+                options.Port = serverConfiguration.Port;
+                options.PacketProcessor = new MinecraftPacketProcessor();
+                options.ReceiveStrategy = ReceiveStrategyType.Queued;
+            });
+        });
+    })
+    .UseConsoleLifetime()
+    .Build();
 
-            return serverHost.RunAsync();
-        }
-    }
-}
+await serverHost.RunAsync();
